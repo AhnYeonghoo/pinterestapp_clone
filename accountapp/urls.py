2c96b196d7c21972e5e132e9aaa1b3865aa590f5
AhnYeonghoo <dksdudgn00@naver.com>
2022-12-22 17:37:55 +0900

    Login & Logout Impl

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'accountapp'
 
@@ -9,4 +10,6 @@ app_name = 'accountapp'
 urlpatterns = [
     path('hello_world', views.hello_world, name='hello_world'),
     path('create/', views.AccountCreateView.as_view() ,name='create'),
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout' ),
 ]
--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 import environ
 from pathlib import Path
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,8 +57,12 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    'bootstrap4',
     "django.contrib.staticfiles",
     "accountapp",
+    
+
+
 ]
 
 MIDDLEWARE = [
@@ -147,3 +152,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
