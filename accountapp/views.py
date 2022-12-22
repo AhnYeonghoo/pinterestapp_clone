1b8cbbd4079bd4723a1e3eba61337c72e306ee7e
AhnYeonghoo <dksdudgn00@naver.com>
2022-12-22 18:08:19 +0900

    회원정보 변경 및 회원탈퇴 기능 구현

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.http import HttpResponse
 from django.urls import reverse, reverse_lazy
 from accountapp.models import HelloWorld
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .forms import AccountUpdateForm
 # Create your views here.
 
 
@@ -31,3 +32,21 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
+
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
