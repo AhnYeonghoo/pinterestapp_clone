19585307e1d51db0a9cfab41fd8e9d632c3f628c
AhnYeonghoo <dksdudgn00@naver.com>
2022-12-23 14:33:04 +0900

    Authenticated impl

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,25 +6,27 @@ from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import AccountUpdateForm
+from django.contrib.auth.decorators import login_required
+from .decorators import account_ownership_required
+from django.utils.decorators import method_decorator
 # Create your views here.
 
+has_ownership = [account_ownership_required, login_required]
 
+@login_required
 def hello_world(request):
 
     if request.method == "POST":
-
         temp = request.POST.get('hello_world_input')
-        
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
-
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
 
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-
+    
     
 
 class AccountCreateView(CreateView):
@@ -40,6 +42,8 @@ class AccountDetailView(DetailView):
     template_name = 'accountapp/detail.html'
 
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
@@ -47,6 +51,10 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
+
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
